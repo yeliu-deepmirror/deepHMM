ed08543b81eb0d356946020283c07709653fe148
yeliu <yeliu@deepmirror.com>
2024-03-01 10:56:31 +0800

    learning HMM based on this

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 """
 An implementation of a Deep Markov Model in Pyro based on reference [1].
 
-Adopted from https://github.com/uber/pyro/tree/dev/examples/dmm  
+Adopted from https://github.com/uber/pyro/tree/dev/examples/dmm
          and https://github.com/clinicalml/structuredinference
 
 Reference:
@@ -47,15 +47,15 @@ def main(args):
     timestamp = datetime.now().strftime('%Y%m%d%H%M')
     tb_writer = SummaryWriter("./output/{}/{}/{}/logs/".format(args.model, args.expname, args.dataset)\
                           +timestamp) if args.visual else None
-    
+
     config=getattr(configs, 'config_'+args.model)()
-    
+
     # instantiate the dmm
     model = getattr(models, args.model)(config)
     model = model.cuda()
     if args.reload_from>=0:
         load_model(model, args.reload_from)
-        
+
     train_set=PolyphonicDataset(args.data_path+'train.pkl')
     valid_set=PolyphonicDataset(args.data_path+'valid.pkl')
     test_set=PolyphonicDataset(args.data_path+'test.pkl')
@@ -65,33 +65,33 @@ def main(args):
     #################
     times = [time.time()]
     for epoch in range(config['epochs']):
-            
+
         train_loader=torch.utils.data.DataLoader(dataset=train_set, batch_size=config['batch_size'], shuffle=True, num_workers=1)
         train_data_iter=iter(train_loader)
         n_iters=train_data_iter.__len__()
-        
+
         epoch_nll = 0.0 # accumulator for our estimate of the negative log likelihood (or rather -elbo) for this epoch
-        i_batch=1   
+        i_batch=1
         n_slices=0
         loss_records={}
-        while True:            
-            try: x, x_rev, x_lens = train_data_iter.next()                  
-            except StopIteration: break # end of epoch                 
+        while True:
+            try: x, x_rev, x_lens = next(train_data_iter)                  
+            except StopIteration: break # end of epoch
             x, x_rev, x_lens = gVar(x), gVar(x_rev), gVar(x_lens)
-            
-            if config['anneal_epochs'] > 0 and epoch < config['anneal_epochs']: # compute the KL annealing factor            
+
+            if config['anneal_epochs'] > 0 and epoch < config['anneal_epochs']: # compute the KL annealing factor
                 min_af = config['min_anneal']
                 kl_anneal = min_af+(1.0-min_af)*(float(i_batch+epoch*n_iters+1)/float(config['anneal_epochs']*n_iters))
-            else:            
+            else:
                 kl_anneal = 1.0 # by default the KL annealing factor is unity
-            
+
             loss_AE = model.train_AE(x, x_rev, x_lens, kl_anneal)
-            
+
             epoch_nll += loss_AE['train_loss_AE']
             i_batch=i_batch+1
             n_slices=n_slices+x_lens.sum().item()
-            
-        loss_records.update(loss_AE)   
+
+        loss_records.update(loss_AE)
         loss_records.update({'epo_nll':epoch_nll/n_slices})
         times.append(time.time())
         epoch_time = times[-1] - times[-2]
@@ -104,7 +104,7 @@ def main(args):
         if (epoch+1) % args.test_freq == 0:
             save_model(model, epoch)
             test_loader=torch.utils.data.DataLoader(dataset=test_set, batch_size=config['batch_size'], shuffle=False, num_workers=1)
-            for x, x_rev, x_lens in test_loader: 
+            for x, x_rev, x_lens in test_loader:
                 x, x_rev, x_lens = gVar(x), gVar(x_rev), gVar(x_lens)
                 test_nll = model.valid(x, x_rev, x_lens) / x_lens.sum()
             log("[val/test epoch %08d]  %.8f" % (epoch, test_nll))
@@ -124,6 +124,6 @@ if __name__ == '__main__':
     parser.add_argument('--jit', action='store_true')
     parser.add_argument('-l', '--log', type=str, default='dmm.log')
     args = parser.parse_args()
-    
+
     os.makedirs(f'./output/{args.model}/{args.expname}/{args.dataset}/models', exist_ok=True)
     main(args)
